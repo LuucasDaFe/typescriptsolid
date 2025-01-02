1afb47e5b921906154167b0aa1e956f01b0d1349
Lucas Abraao <Labraao957@gmail.com>
2025-01-02 02:51:13 -0300

    OCP

---
+++ src/ocp/main.ts
@@ -0,0 +1,29 @@
+/*
+open/closed principle
+Entidades devem estar abertas para extensão, fechada para modificaçãõ.
+*/
+import { Messaging } from "./services/messaging";
+import { Order } from "./classes/order";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/product";
+import { ShoppingCart } from "./classes/shopping.cart";
+import { NoDiscount } from "./classes/discount";
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product("A", 49.9));
+shoppingCart.addItem(new Product("B", 9.9));
+shoppingCart.addItem(new Product("C", 29.9));
+
+console.log(shoppingCart.itens);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDicount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
